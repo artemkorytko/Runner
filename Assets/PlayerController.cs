c9fbdd35645e2688e0c55d6d7a56d89881c6c85a
artemkorytko <karytich5@gmail.com>
2021-11-30 22:14:11 +0300

    add base logic

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string IDLE = "Idle";
+    private const string RUN = "Run";
+    private const string FALL = "Fall";
+    private const string DANCE = "Dance";
+    private bool isSeed;
+
+    private Animator animator;
+
+    public event Action OnFinish;
+    public event Action OnDied;
+
+    private bool isActive;
+    [SerializeField] private float moveSpeed = 10;
+
+    public bool IsActive
+    {
+        get => isActive;
+
+        set
+        {
+            if(value == true)
+            {
+                animator.SetTrigger(RUN);
+            }
+            isActive = value;
+        }
+    }
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!IsActive)
+            return;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
+    }   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.CompareTag("Wall") 
+            && collision.gameObject.layer == 6 
+            && collision.gameObject.GetComponent<Wall>())
+        {
+           Died();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.CompareTag("Finish"))
+        {
+            Finish();
+        }
+    }
+
+    private void Died()
+    {
+        animator.SetTrigger(FALL);
+        IsActive = false;
+        OnDied?.Invoke();
+    }
+
+    private void Finish()
+    {
+        animator.SetTrigger(DANCE);
+        IsActive = false;
+        OnFinish?.Invoke();
+    }
+}
